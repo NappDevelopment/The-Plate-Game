205eab82aea7fcc5b738b1599c29d8828d1042d2
Connor Krupp <connor@connorkrupp.com>
2016-10-16 19:37:37 -0400

    Removed storyboard

--- The Plate Game/StateTableViewCell.swift
+++ The Plate Game/StateTableViewCell.swift
@@ -11,8 +11,36 @@ import UIKit
 class StateTableViewCell: UITableViewCell {
     static let identifier = "StateCell"
     
-    @IBOutlet weak var stateNameLabel: UILabel!
-    @IBOutlet weak var stateNicknameLabel: UILabel!
+    var stateNameLabel = UILabel()
+    var stateNicknameLabel = UILabel()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let stackView = UIStackView(arrangedSubviews: [stateNameLabel, stateNicknameLabel])
+
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stateNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        stateNicknameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        stateNicknameLabel.setContentCompressionResistancePriority(UILayoutPriorityDefaultLow, for: .vertical)
+        stateNicknameLabel.setContentHuggingPriority(UILayoutPriorityDefaultHigh, for: .vertical)
+
+        self.addSubview(stackView)
+        
+        let margins = self.layoutMarginsGuide
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: self.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
     
     var stateName: String? {
         didSet {
